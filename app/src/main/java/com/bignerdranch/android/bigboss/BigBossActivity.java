933fb1173b112a3008902d420a4ba66f16123631
Careyjia <1124531497@qq.com>
2017-04-15 15:41:04 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/bigboss/BigBossActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.bigboss;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BigBossActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_big_boss);
+    }
+}
